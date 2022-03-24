a7ae1f25dbd96335e9233a83ffe83022b957cc14
mohsen jahangiri <moh7seven@yahoo.com>
2022-03-25 02:24:51 +0430

    homejsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import Navbar from './componants/Navbar'
 import Home from './componants/Home';
+import { Switch, Rout } from 'react-router-dom'
+import Products from './componants/Products';
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Navbar />
+      <Switch>
+        <Rout exact path="/" component={Home} />
+        <Rout exact path="/products" component={Products} />
+        <Home />
+      </Switch>
     </>
   );
 }
--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -3,36 +3,36 @@ import React from "react";
 const Navbar=()=>{
     return(
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-  <div class="container">
-    <a class="navbar-brand fw-bold fs-4" href="#">LA COLECTION</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+  <div className="container">
+    <a className="navbar-brand fw-bold fs-4" href="#">LA COLECTION</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">products</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">products</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">about</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">about</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">contact</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">contact</a>
         </li>
       </ul>
       <div className="buttons">
         <a href="" className="btn btn-outline-dark">
-           <i class="fa fa-sign-in me-1" aria-hidden="true">Login</i>
+           <i className="fa fa-sign-in me-1" aria-hidden="true">Login</i>
         </a>
         <a href="" className="btn btn-outline-dark ms-2">
-           <i class="fa fa-user-plus me-1" aria-hidden="true">Register</i>
+           <i className="fa fa-user-plus me-1" aria-hidden="true">Register</i>
         </a>
         <a href="" className="btn btn-outline-dark ms-2">
-           <i class="fa fa-shopping-cart me-1" aria-hidden="true">Cart(0)</i>
+           <i className="fa fa-shopping-cart me-1" aria-hidden="true">Cart(0)</i>
         </a>
       </div>
     </div>
