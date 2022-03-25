bf0c4a31bd4cebb7ace71d5d59a0d79b6d1b370d
mohsen jahangiri <moh7seven@yahoo.com>
2022-03-25 15:06:49 +0430

    addswitch

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@
 import './App.css';
 import Navbar from './componants/Navbar'
 import Home from './componants/Home';
-import { Switch, Rout } from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom'
 import Products from './componants/Products';
+import Product from './componants/Product';
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Rout exact path="/" component={Home} />
-        <Rout exact path="/products" component={Products} />
-        <Home />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={Product} />
       </Switch>
     </>
   );
--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,39 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar=()=>{
+  const state=useSelector((state)=>state.handleCart)
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
   <div className="container">
-    <a className="navbar-brand fw-bold fs-4" href="#">LA COLECTION</a>
+    <NavLink className="navbar-brand fw-bold fs-4" to="/">LA COLECTION</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">products</a>
+          <NavLink className="nav-link" to="/products">products</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">about</a>
+          <NavLink className="nav-link" to="/about">about</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">contact</a>
+          <NavLink className="nav-link" to="/contact">contact</NavLink>
         </li>
       </ul>
       <div className="buttons">
-        <a href="" className="btn btn-outline-dark">
-           <i className="fa fa-sign-in me-1" aria-hidden="true">Login</i>
-        </a>
-        <a href="" className="btn btn-outline-dark ms-2">
-           <i className="fa fa-user-plus me-1" aria-hidden="true">Register</i>
-        </a>
-        <a href="" className="btn btn-outline-dark ms-2">
-           <i className="fa fa-shopping-cart me-1" aria-hidden="true">Cart(0)</i>
-        </a>
+        <NavLink to="/login" className="btn btn-outline-dark">
+           <i className="fa fa-sign-in me-1" ></i>Login
+        </NavLink>
+        <NavLink to="/register" className="btn btn-outline-dark ms-2">
+           <i className="fa fa-user-plus me-1" ></i>Register
+        </NavLink>
+        <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+           <i className="fa fa-shopping-cart me-1" ></i>Cart({state.length})</NavLink>
       </div>
     </div>
   </div>
